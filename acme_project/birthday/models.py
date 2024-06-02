b55d129e3cdc5c8ba352441f93e26b816be1c2f0
Andrey Starostin <161159835+a-lessnick@users.noreply.github.com>
2024-06-02 14:51:55 +0300

    Спринт 9/18, Тема 2/3, Урок 10/10

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,10 +1,29 @@
 # birthday/models.py
 from django.db import models
 
+from .validators import real_age
+
 
 class Birthday(models.Model):
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
+
     first_name = models.CharField('Имя', max_length=20)
+
     last_name = models.CharField(
         'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
     )
-    birthday = models.DateField('Дата рождения')
+
+    birthday = models.DateField('Дата рождения',
+                                validators=(real_age,))
+
+    image = models.ImageField('Фото',
+                              upload_to='birthdays_images',
+                              blank=True)
+
+
